5f17aa6efb262ac8d8efa0db43a67469017b7b83
Sandra | Laila | (Priscilla) <a.g.marosi@bluewin.ch>
2024-11-27 15:30:42 +0100

    Update sw.js

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './balls.js',
-  './balls_bg.wasm',
+  './grid_path_finder.js',
+  './grid_path_finder_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
